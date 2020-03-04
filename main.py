4f11a429297e300a5b79c5900d144427e5e25a47
Lambert Teisserenc <lambert.teisserenc@braineet.com>
2020-03-04 14:12:30 +0100

    improve get map

--- main.py
+++ main.py
@@ -10,18 +10,18 @@ import json
 hote = "aerotoulousain.fr"
 port = 3333
 
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
 socket.connect((hote, port))
 print("Connection on {}".format(port))
 
 # set up pygame
 pygame.init()
 
-SCREEN_SIZE_X = 100
-SCREEN_SIZE_Y = 100
+SCREEN_SIZE_X = 500
+SCREEN_SIZE_Y = 500
 
-CELL_SIZE_X = SCREEN_SIZE_X / 20
-CELL_SIZE_Y = SCREEN_SIZE_Y / 20
+CELL_SIZE_X = SCREEN_SIZE_X / 100
+CELL_SIZE_Y = SCREEN_SIZE_Y / 100
 
 # set up the window
 windowSurface = pygame.display.set_mode((SCREEN_SIZE_X, SCREEN_SIZE_Y), 0, 32)
@@ -47,22 +47,32 @@ pygame.display.update()
 
 x = 0
 
+buffer_str = ""
+
+
+def get_map():
+    global buffer_str
+    while not ";" in buffer_str:
+        buffer = socket.recv(1024 * 48)
+        buffer_str += buffer.decode("utf-8")
+
+    strs = str.split(buffer_str, ";")
+    data_str = strs[0]
+
+    buffer_str = ""
+    i = 1
+    while i + 1 < len(strs):
+        buffer_str += strs[i + 1]
+
+    return data_str
 
 def pull_map():
     while True:
-        socket.send(b"GET MAP;")
-
-        buffer = socket.recv(1024*48)
-        data = buffer
-        while len(buffer) == 1024*48:
-            buffer = socket.recv(1024*48)
-            data += buffer
-
+        data_str = get_map()
         data_json = []
         try:
-            data_str = data.decode("utf-8")
-            data_strs = data_str.split(";")
-            data_json = json.loads(data_strs[0])
+            # print(data_str)
+            data_json = json.loads(data_str)
 
             windowSurface.fill(BLACK)
             for x in range(len(data_json)):
@@ -103,6 +113,8 @@ while True:
             socket.sendall(arr)
         if event.type == MOUSEBUTTONUP:
             draging = False
+        if event.type == KEYDOWN:
+            socket.send(b"TOGGLE;")
         if event.type == MOUSEMOTION and draging:
             pos = pygame.mouse.get_pos()
             pos_x = int(pos[0] // CELL_SIZE_X)
